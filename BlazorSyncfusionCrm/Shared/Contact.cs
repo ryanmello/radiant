f029a3726743c61da503107df5ac3b668f40c21d
Ryan Mello <74434148+ryanmello@users.noreply.github.com>
2023-03-28 09:24:14 -0700

    css and validation

--- BlazorSyncfusionCrm/Shared/Contact.cs
+++ BlazorSyncfusionCrm/Shared/Contact.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -12,6 +13,7 @@ namespace BlazorSyncfusionCrm.Shared
         public int Id { get; set; }
 		public string FirstName { get; set; } = string.Empty;
 		public string LastName { get; set; } = string.Empty;
+		[Required]
 		public string NickName { get; set; } = string.Empty;
 		public string Place { get; set; } = string.Empty;
 		public bool isDeleted { get; set; }
